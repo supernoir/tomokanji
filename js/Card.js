0c15ea730f6cec509d9b12812100aeadf43639ab
supernoir <kawohl.bentodesign@gmail.com>
2018-02-08 22:47:05 +0100

    restyles card

--- js/Card.js
+++ js/Card.js
@@ -18,8 +18,8 @@ export default class Card extends Component {
 
   render() {
     return (
-      <div className="card">
-        <div key={1} className="card_body">
+      <div key={1} className="card">
+        <div className="card-head">
           <div className="card-kunyomi">
             <p className="card-reading-type">訓読み</p>
             <p className="card-reading-content">ふゆ</p>
@@ -28,17 +28,13 @@ export default class Card extends Component {
             <p className="card-reading-type">音読み</p>
             <p className="card-reading-content">とう</p>
           </div>
+        </div>
+        <div className="card-body">
           <div className="card-kanji">
             <h1 className="card-kanji-content">冬</h1>
           </div>
-          <div className="card-romanization">
-            <p className="card-romanization-onyomi">
-              <span className="label-onyomi">ON</span> tō
-            </p>
-            <p className="card-romanization-kunyomi">
-              <span className="label-kunyomi">KUN</span> fuyu
-            </p>
-          </div>
+        </div>
+        <div className="card-footer">
           <div className="card-meaning">Winter</div>
           <div className="card-details">Grade 2 "Kyōiku" kanji</div>
         </div>
--- js/index.js
+++ js/index.js
@@ -8,22 +8,30 @@ import CardData from '../data/example.json'
 import '../css/main.scss'
 
 const Logo = () => {
-  return <p className="logo">友漢字</p>
+  return (
+    <div className="brand">
+      <p className="logo">
+        TOMO
+        <span className="highlight">KANJI</span>
+        {' '}
+        友
+        <span className="highlight">カンジ</span>
+      </p>
+      <p className="tagline">友達の漢字</p>
+    </div>
+  )
 }
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="app">
+        <header className="header"><Logo /></header>
         <div className="page">
-          <header className="header"><Logo /></header>
-          <div className="page-content">
-            <div className="card">
-              <Card data={CardData} />
-            </div>
-            <aside><a href="#" className="button">Next!</a></aside>
-          </div>
+          <Card data={CardData} />
         </div>
+        <aside><a href="#" className="button">Next!</a></aside>
+
       </div>
     )
   }
