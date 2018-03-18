bc478f8bcad29ee3629769131bb429cee525dd38
supernoir <kawohl.bentodesign@gmail.com>
2018-03-18 23:48:40 +0100

    adds jest, procfile

---
+++ js/test/label.spec.js
@@ -0,0 +1,14 @@
+test('render a label', () => {
+	const wrapper = shallow(<Label>Hello Jest!</Label>);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('render a small label', () => {
+	const wrapper = shallow(<Label small>Hello Jest!</Label>);
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('render a grayish label', () => {
+	const wrapper = shallow(<Label light>Hello Jest!</Label>);
+	expect(wrapper).toMatchSnapshot();
+});
